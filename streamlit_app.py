a46ac3192c10dd76f90b80a352f8d015591a0c17
Ls9bg <168305674+Ls9bg@users.noreply.github.com>
2025-10-07 10:36:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 #streamlit.title('My parents New Healthy Diner')
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col,when_matched
-
+import requests
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -27,16 +27,15 @@ ingredients_list=st.multiselect(
     ,my_dataframe
     ,max_selections=5
 )
-#new section to displaysmoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
+
 if ingredients_list:
     ingredients_string= ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string +=fruit_chosen +' '
-
+       ingredients_string +=fruit_chosen +' '
+       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+       sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+ name_on_order + """')"""
