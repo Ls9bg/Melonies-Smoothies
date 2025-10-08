0c20ee440f52b3496d20c0e37920731e5e6c387b
Ls9bg <168305674+Ls9bg@users.noreply.github.com>
2025-10-08 14:41:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,7 @@ st.write("The name on your smoothie will be", name_on_order)
 cnx=st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list=st.multiselect(
     'Choose up to 5 ingredients:'
     ,my_dataframe
@@ -34,11 +33,11 @@ if ingredients_list:
     ingredients_string= ''
     for fruit_chosen in ingredients_list:
       ingredients_string +=fruit_chosen +' '
-      #search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+      search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
       #st.write ('The search value for ',fruit_chosen,' is ' ,search_on,'.')
       st.subheader(fruit_chosen+' Nutrition Information')
-      
       smoothiefroot_response=requests.get("https://SmoothieFroot.com/api/fruit/fruit_chosen/fruit_chosen")
+      sf_df=st.dataframe(data=smoothiefroot_response.json())  
     my_insert_statement = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+ name_on_order + """')"""
        
