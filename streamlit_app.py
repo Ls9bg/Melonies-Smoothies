8d6675b7a188b410b86bd908557e0514018e866b
Ls9bg <168305674+Ls9bg@users.noreply.github.com>
2025-10-08 12:48:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,5 +48,5 @@ if ingredients_list:
     time_to_insert=st.button('Submit Order')
     
     if time_to_insert:
-        session.sql(my_insert_stmt).collect()
+        session.sql(my_insert_statement).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
